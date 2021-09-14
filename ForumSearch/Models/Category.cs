24ac213869e3b984ecf6d7a929945abed0a46a91
Labocania <hugokl@outlook.com>
2021-09-14 17:57:15 -0300

    Created models and first version of json file with data. Added NuGet package System.text.json for deserialization.

---
+++ ForumSearch/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ForumSearch.Models
+{
+    public class Category
+    {
+        public string CategoryName { get; set; }
+        public ICollection<Forum> ForumList { get; set; }
+    }
+}
---
+++ ForumSearch/Models/Forum.cs
@@ -0,0 +1,8 @@
+﻿namespace ForumSearch.Models
+{
+    public class Forum
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
